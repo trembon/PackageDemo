8cbedc46328ac2411fc3d05c10625783a9e0bd1d
trembon <trembon@hotmail.com>
2023-12-10 22:58:03 +0100

    change namespace format

--- src/PackageDemo/PackageDemo.Tests/Services/TrackingNumberServiceTests.cs
+++ src/PackageDemo/PackageDemo.Tests/Services/TrackingNumberServiceTests.cs
@@ -1,58 +1,57 @@
 ﻿using PackageDemo.Services;
 
-namespace PackageDemo.Tests.Services
+namespace PackageDemo.Tests.Services;
+
+public class TrackingNumberServiceTests
 {
-    public class TrackingNumberServiceTests
+    [Theory]
+    [InlineData("999468372030010241")]
+    [InlineData("999468312345678902")]
+    public void TryParse_Should_Parse_Valid_Tracking_Numbers(string trackingNumber)
     {
-        [Theory]
-        [InlineData("999468372030010241")]
-        [InlineData("999468312345678902")]
-        public void TryParse_Should_Parse_Valid_Tracking_Numbers(string trackingNumber)
+        Fixture fixture = new();
+
+        bool result = fixture.Service.TryParse(trackingNumber, out var parsed);
+
+        Assert.True(result);
+        Assert.Equal(trackingNumber, parsed.ToString());
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData(null)]
+    [InlineData("123451")]
+    [InlineData("888468312345678902")]
+    [InlineData("8884683I23456789O2")]
+    public void TryParse_Should_Fail_When_Rules_Are_Not_Followed(string trackingNumber)
+    {
+        Fixture fixture = new();
+
+        bool result = fixture.Service.TryParse(trackingNumber, out var parsed);
+
+        Assert.False(result);
+        Assert.Equal(-1, parsed);
+    }
+
+    [Fact]
+    public void GenerateNew_Should_Generate_Valid_Tracking_Number()
+    {
+        Fixture fixture = new();
+
+        long result = fixture.Service.GenerateNew();
+
+        Assert.True(result > 0);
+        Assert.Equal(18, result.ToString().Length);
+        Assert.Equal("999", result.ToString()[..3]);
+    }
+
+    private class Fixture
+    {
+        public TrackingNumberService Service { get; }
+
+        public Fixture()
         {
-            Fixture fixture = new();
-
-            bool result = fixture.Service.TryParse(trackingNumber, out var parsed);
-
-            Assert.True(result);
-            Assert.Equal(trackingNumber, parsed.ToString());
-        }
-
-        [Theory]
-        [InlineData("")]
-        [InlineData(null)]
-        [InlineData("123451")]
-        [InlineData("888468312345678902")]
-        [InlineData("8884683I23456789O2")]
-        public void TryParse_Should_Fail_When_Rules_Are_Not_Followed(string trackingNumber)
-        {
-            Fixture fixture = new();
-
-            bool result = fixture.Service.TryParse(trackingNumber, out var parsed);
-
-            Assert.False(result);
-            Assert.Equal(-1, parsed);
-        }
-
-        [Fact]
-        public void GenerateNew_Should_Generate_Valid_Tracking_Number()
-        {
-            Fixture fixture = new();
-
-            long result = fixture.Service.GenerateNew();
-
-            Assert.True(result > 0);
-            Assert.Equal(18, result.ToString().Length);
-            Assert.Equal("999", result.ToString()[..3]);
-        }
-
-        private class Fixture
-        {
-            public TrackingNumberService Service { get; }
-
-            public Fixture()
-            {
-                Service = new TrackingNumberService();
-            }
+            Service = new TrackingNumberService();
         }
     }
 }
